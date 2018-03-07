3efeb436e6843f6e4c60c4a76ec2d422700e1e04
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-03-07 19:31:00 +0530

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,25 @@
+int led = 13;
+int value = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    value = Serial.read();
+    delay(5);
+
+    if (value == '1') {
+      digitalWrite(led, HIGH);
+      Serial.println("LED is ON");
+    }
+
+    if (value == '0') {
+      digitalWrite(led, LOW);
+      Serial.println("LED is OFF");
+    }
+  }
+}
